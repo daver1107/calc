7d24425404785124e851d03f2d3ac11d419d1288
AntonDavydov <daver1107@yahoo.com>
2021-04-30 22:07:02 -0700

    new calculator created

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input: ");
+        double num = scanner.nextDouble();
+        char operation = scanner.next().charAt(0);
+        double num2 = scanner.nextDouble();
+        if (num2 == 0 && operation == '/') {
+            System.out.println("Ошибка! Делить на 0 нельзя");
+
+        } else {
+            System.out.println(total(num, operation, num2));
+        }
+        scanner.close();
+    }
+
+    private static double total(double num, char operation, double num2) {
+        double result = 0;
+        switch (operation) {
+            case '-' -> result = num - num2;
+            case '+' -> result = num + num2;
+            case '/' -> result = num / num2;
+            case '*' -> result = num * num2;
+        }
+        return result;
+    }
+}
